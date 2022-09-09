fb550c6d5a49bc2c27608b7e545fe75366172e71
chemutai_viola <chemuvayoo@gmail.com>
2022-09-08 17:01:36 -0700

    betty lint 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print the size of various data types
- * Return: 0 if exited properly, non-zero otherwise
- */
+* main - print the size of various data types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 
 {
@@ -11,8 +11,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-       	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-
 }
