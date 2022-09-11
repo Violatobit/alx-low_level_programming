66594630ce94c0af4ab571e8ecb13cff6b4907a8
chemutai_viola <chemuvayoo@gmail.com>
2022-09-11 12:59:00 -0700

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,3 @@
-/*
- *File: 1-last_digit.c
- *Auth: viola chemutai
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -26,7 +21,7 @@ int main(void)
 				n, n % 10);
 		}
 		else if ((n % 10) < 6 && (n % 10) != 0)
-		}
+		{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n",
 				n, n % 10);
 		}
