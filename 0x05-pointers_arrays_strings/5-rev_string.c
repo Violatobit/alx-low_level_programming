b9b3c77e64495db7097f58a89524df491c8a876a
chemutai_viola <chemuvayoo@gmail.com>
2022-09-20 04:41:32 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,21 +2,21 @@
 
 /**
  *rev_string - Prints a string in reverse.
- *@s: Reverse string to be printed.
+ *@mystr: Reverse string to be printed.
  */
-void rev_string(char *s)
+void rev_string(char *mystr)
 {
 	int length = 0;
 	int index = 0;
 
-	while (s[index++])
+	while (mystr[index++])
 	{
 		length++;
 	}
 	for (index = length - 1; index >= 0; index--)
 	{
 
-		_putchar(s[index]);
+		_putchar(mystr[index]);
 
 	}
 
