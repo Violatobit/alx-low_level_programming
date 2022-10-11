b06cf5657611b5b2dd657bb006d18a096797e4a6
chemutai_viola <chemuvayoo@gmail.com>
2022-10-11 07:27:30 -0700

    Defining a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+*init_dog - Creating a Dog.
+*@x: name of struct
+*@name: Name of Dog.
+*@age: Age of Dog.
+*@owner: owner of Dog.
+* Return: Always 0
+*/
+void init_dog(struct dog *x, char *name, float age, char *owner)
+{
+	if (x)
+	{
+		x->name = name;
+		x->age = age;
+		x->owner = owner;
+	}
+	else
+	{
+		return;
+	}
+}
